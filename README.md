5a106f9557a71705b94377cd7929286e00c00112
Lennart-321 <lnnrt0@gmail.com>
2024-01-10 13:43:30 +0100

    Uppgifter JS - Variables and Datatypes (v1.0)

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-var-and-types
+
+Uppgifter JS - Variables and Datatypes
---
+++ index.js
@@ -0,0 +1,62 @@
+//0: console.log("Hello World!");
+
+// //1.
+// let age = 61;
+// console.log(age + " is my age");
+
+// //2.
+// let a = 0;
+// const b = 1;
+// var c = 2;
+// console.log(a, b, c);
+
+// //3.
+// let e = "1";
+// let f = 1;
+// let g = true;
+// console.log(e, f, g);
+
+// //4.
+// const name = "Lennart";
+// console.log(name);
+// name = "L";
+// console.log(name);
+
+// //7.
+// const number1 = 1;
+// const number2 = 2;
+// const number3 = number1 + number2;
+// console.log(number3);
+
+// //8.
+// const result1 = number1 - number2;
+// const result2 = number1 * number2;
+// const result3 = number1 / number2;
+// console.log(result1, " ", result2, " ", result3);
+
+// //9.
+// let a = 17;
+// console.log(a);
+// a = a + 5;
+// console.log(a);
+
+//10.
+let a = 10;
+a = a - 4;
+a = 7;
+a = a + 2;
+console.log(a);
+
+// "use strict";
+// let f;
+// if (true) {
+//   let a = 5;
+//   f = () => a;
+// }
+
+// //a = a + 1;
+
+// const a = "Hello World";
+
+// console.log("- " + a + " " + typeof NaN + " " + f());
+// console.log("- ", typeof Infinity);
